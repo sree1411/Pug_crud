f927d1647332e2aa0fa8b82e7b0fcb56f79b274a
sreekanth <sreekanth>
2024-12-04 09:39:34 +0530

    cookies

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 
 
 
-router.get('/reviews', (req,res)=>{
+router.get('/', (req,res)=>{
     let reviews = JSON.parse(fs.readFileSync("reviews.txt").toString())
     res.render("home", {reviews})
  })
--- server.js
+++ server.js
@@ -2,19 +2,61 @@ const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
 const fs = require('fs');
-const port = 5000 ;
+
 const viewRouter  = require('./routes/reviewRoutes')
-
+var cookieParser = require('cookie-parser')
+var user = JSON.parse(fs.readFileSync("users.txt").toString());
 app.set('view engine', 'pug')
-
+const port = 5000 ;
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-
+app.use(cookieParser())
  
 
 app.use(express.static(__dirname+'/public'))
 
-app.use('/', viewRouter )
+
+
+
+app.post('/login', (req,res)=>{
+  res.cookie("username", req.body.username)
+  res.cookie("password", req.body.password)
+  res.redirect('/')
+})
+
+ 
+app.use((req, res, next) => {
+    console.log("Cookies received:", req.cookies);
+    if (req.cookies.username) {
+        let use = user.find((us) =>
+            us.username === req.cookies.username &&
+            us.password === req.cookies.password
+        );
+        console.log("Matched user:", use); // Log the result of `find`.
+        if (use) {
+            next();
+        } else {
+            console.log("Invalid credentials. Redirecting to login.");
+            res.redirect('/login.html');
+        }
+    } else {
+        console.log("No cookies found. Redirecting to login.");
+        res.redirect('/login.html');
+    }
+});
+
+
+
+app.use('/reviews', viewRouter )
+
+
+
+app.get('/students', (req,res)=>{
+    res.cookie("mycookies", "darkfantasy")
+    res.send('/')
+})
+
+
 
 
 
